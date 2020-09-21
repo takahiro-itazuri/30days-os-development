568ba635b66c77d0b28b715b09c062c6920d19d7
takahiro-itazuri <takahiro.lab.1226@gmail.com>
2020-09-22 03:29:15 +0900

    add uppercase support for sprintf

--- day07/harib04f/mystdio.c
+++ day07/harib04f/mystdio.c
@@ -2,8 +2,8 @@
 #include "mystdio.h"
 
 /* 10進数からASCIIコードに変換 */
-int dec2asc(char *str, int dec) {
-	int len, len_buf = 0;
+int dec2asc(char *str, int dec, int len) {
+	int len_buf = 0, tmp;
 	int buf[10];
 	
 	for (;;) {
@@ -11,18 +11,34 @@ int dec2asc(char *str, int dec) {
 		if (dec < 10) break;
 		dec /= 10;
 	}
-	len = len_buf;
+	
+	if (len == 0) {
+		len = len_buf;
 
-	while (len_buf) {
-		*(str++) = buf[--len_buf] + 0x30;
+		while (len_buf) {
+			*(str++) = buf[--len_buf] + '0';
+		}
+	}
+	else {
+		tmp = len;
+
+		while (tmp) {
+			if (tmp > len_buf) {
+				--tmp;
+				*(str++) = 0x30;
+			}
+			else {
+				*(str++) = buf[--tmp] + '0';
+			}
+		}
 	}
 
 	return len;
 }
 
 /* 16進数からASCIIコードに変換 */
-int hex2asc(char *str, int dec) {
-	int len, len_buf = 0;
+int hex2asc(char *str, int dec, int len, int upper) {
+	int len_buf = 0, tmp;
 	int buf[10];
 
 	for (;;) {
@@ -30,11 +46,30 @@ int hex2asc(char *str, int dec) {
 		if (dec < 16) break;
 		dec /= 16;
 	}
-	len = len_buf;
 
-	while (len_buf) {
-		--len_buf;
-		*(str++) = (buf[len_buf] < 10) ? (buf[len_buf] + 0x30) : (buf[len_buf] - 9 + 0x60);
+	if (len == 0) {
+		len = len_buf;
+
+		while (len_buf) {
+			--len_buf;
+			if (upper) *(str++) = (buf[len_buf] < 10) ? (buf[len_buf] + '0') : (buf[len_buf] + 'A');
+			else *(str++) = (buf[len_buf] < 10) ? (buf[len_buf] + '0') : (buf[len_buf] + 'a');
+		}
+	}
+	else {
+		tmp = len;
+
+		while (tmp) {
+			if (tmp > len_buf) {
+				--tmp;
+				*(str++) = '0';
+			}
+			else {
+				--tmp;
+				if (upper) *(str++) = (buf[tmp] < 10) ? (buf[tmp] + '0') : (buf[tmp] - 10 + 'A');
+				else *(str++) = (buf[tmp] < 10) ? (buf[tmp] + '0') : (buf[tmp] - 10 + 'a');
+			}
+		}
 	}
 
 	return len;
@@ -49,13 +84,20 @@ void mysprintf(char *str, char *fmt, ...) {
 	while (*fmt) {
 		if (*fmt == '%') {
 			++fmt;
-			switch (*fmt) {
+			len = 0;
+			while ('0' <= *fmt && *fmt <= '9') {
+				len = len * 10 + (*fmt - '0');
+				++fmt;
+			}
+			switch(*fmt) {
 			case 'd':
-				len = dec2asc(str, va_arg(args, int));
+				len = dec2asc(str, va_arg(args, int), len);
 				break;
 			case 'x':
-				len = hex2asc(str, va_arg(args, int));
+				len = hex2asc(str, va_arg(args, int), len, 0);
 				break;
+			case 'X':
+				len = hex2asc(str, va_arg(args, int), len, 1);
 			}
 			str += len;
 			++fmt;
