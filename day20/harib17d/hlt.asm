513f85ad6dc992c6261481c5653d9bbc93a746fb
takahiro-itazuri <takahiro.lab.1226@gmail.com>
2020-09-26 23:35:35 +0900

    add day20

---
+++ day20/harib17d/hlt.asm
@@ -0,0 +1,13 @@
+[BITS 32]
+
+    MOV     AL,'h'
+    CALL    2*8:0xc9a
+    MOV     AL,'e'
+    CALL    2*8:0xc9a
+    MOV     AL,'l'
+    CALL    2*8:0xc9a
+    MOV     AL,'l'
+    CALL    2*8:0xc9a
+    MOV     AL,'o'
+    CALL    2*8:0xc9a
+    RETF
