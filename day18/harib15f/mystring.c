5f072d96cb227361e2701faa3d84b36fdb2cbb9d
takahiro-itazuri <takahiro.lab.1226@gmail.com>
2020-09-26 18:13:02 +0900

    add strncmp

--- day18/harib15f/mystring.c
+++ day18/harib15f/mystring.c
@@ -4,14 +4,47 @@ int mystrcmp(const char *s1, const char *s2)
 {
 	int i = 0, ans = 0;
 	while (s1[i] != 0 && s2[i] != 0) {
-		if (s1[i] != s2[i]) {
-			if (s1[i] > s2[i]) { ans = 1; }
-			else if (s1[i] < s2[i]) { ans = -1; }
+		if (s1[i] == s2[i]) {
+			++i;
+		}
+		else if (s1[i] > s2[i]) {
+			ans = 1;
 			break;
 		}
-		++i;
+		else if (s1[i] < s2[i]) {
+			ans = -1;
+			break;
+		}
 	}
 	if (s1[i] != 0 && s2[i] == 0) { ans = 1; }
 	if (s1[i] == 0 && s2[i] != 0) { ans = -1; }
+	return ans;
+}
+
+int mystrncmp(const char *s1, const char *s2, unsigned int n)
+{
+	int i = 0, ans = 0;
+	while (s1[i] != 0 && s2[i] != 0 && i < n) {
+		if (s1[i] == s2[i]) {
+			++i;
+		}
+		else if (s1[i] > s2[i]) {
+			ans = 1;
+			break;
+		}
+		else if (s1[i] < s2[i]) {
+			ans = -1;
+			break;
+		}
+	}
+
+	if (i != n) {
+		if (s1[i] == 0) {
+			ans = -1;
+		}
+		if (s2[i] == 0) {
+			ans = 1;
+		}
+	}
 	return ans;
 }
--- day18/harib15g/mystring.h
+++ day18/harib15g/mystring.h
@@ -2,5 +2,6 @@
 #define MYSTRING_H
 
 int mystrcmp(const char *s1, const char *s2);
+int mystrncmp(const char *s1, const char *s2, unsigned int n);
 
 #endif /* MYSTRING_H */
